96e16e9d1255daabbd1fd58c1c5ef59feb5f9f87
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-02-27 13:50:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
 st.title("Sweet style")
+
+model = st.selectbox("What would you like to generate?",
+                     ['Text', 'Image'])
+if model == "Text":
+    prompt = st.text_input("Enter your prompt here", "")
+    if st.button('Submit'):
+        pass
+      
+if model == "Image":
+    prompt = st.text_input("Enter your prompt here", "")
+    if st.button('Submit'):
+        pass
