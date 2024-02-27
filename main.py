92834d3e88b4b31d4ca481ecc1d1c99736c9df36
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-02-27 14:01:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ st.title("Sweet style")
 
 model = st.selectbox("What would you like to generate?",
                      ['Text', 'Image'])
-with model:
-    if model == "Text":
-        prompt = st.text_input("Enter your prompt here", "")
-        if st.button('Submit'):
-            pass
-          
-    if model == "Image":
-        prompt = st.text_input("Enter your prompt here", "")
-        if st.button('Submit'):
-            pass
+
+if model == "Text":
+    prompt = st.text_input("Enter your prompt here", "")
+    if st.button('Submit'):
+        pass
+      
+if model == "Image":
+    prompt = st.text_input("Enter your prompt here", "")
+    if st.button('Submit'):
+        pass
