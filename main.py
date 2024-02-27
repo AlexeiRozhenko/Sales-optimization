8d2cf61e81f0d588c73b7cd95364b7c540083c3a
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-02-27 15:24:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import streamlit as st
+from llamaapi import LlamaAPI
+from langchain_experimental.llms import ChatLlamaAPI
+
+llama = LlamaAPI("LL-D0am6EV0l1IeshE15ysSrlPMpqr5Z6rxxaBVgo2bcTvahIR2NZBWhrzkZpPbzoOP")
+model = ChatLlamaAPI(client=llama)
 
 st.title("Sweet style")
 
-model = st.selectbox("What would you like to generate?",
+option = st.selectbox("What would you like to generate?",
                      ['Text', 'Image'])
 
-if model == "Text":
+if option == "Text":
   prompt = st.text_input("Enter your text prompt here", "")
   if st.button('Submit') and not prompt:
-    st.warning("Please enter a prompt", icon="⚠️")
+    st.warning("Please write a prompt", icon="⚠️")
   else:
     pass
       
-elif model == "Image":
+elif option == "Image":
   prompt = st.text_input("Enter your image prompt here", "")
   if st.button('Submit') and not prompt:
-    st.warning("Please enter a prompt", icon="⚠️")
+    st.warning("Please write a prompt", icon="⚠️")
   else:
     pass
