f22b0b4f565c1550c16b2a16471c1f6a7d2d4d86
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-05 17:57:42 +0300

    Update 02_📈_Sales Report.py

--- pages/02_📈_Sales Report.py
+++ pages/02_📈_Sales Report.py
@@ -5,7 +5,7 @@ import os
 
 st.set_page_config(page_title="Sales report", page_icon="📈")
 
-with open('Sales-optimization/pages/sample_file.xlsx','r') as file:
+with open(os.path.join('pages','sample_file.xlsx'),'r') as file:
   st.download_button(label= 'Download Sample File',
                     file_name='sample_file.xlsx',
                     data=file,
