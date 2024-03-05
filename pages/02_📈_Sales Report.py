f28b94d1cfd3f0f7c2a28412a6a7c7d70ba921ff
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-05 17:14:09 +0300

    Update 02_📈_Sales Report.py

--- pages/02_📈_Sales Report.py
+++ pages/02_📈_Sales Report.py
@@ -4,6 +4,6 @@ import plotly.express as px
 
 st.set_page_config(page_title="Sales report", page_icon="📈")
 
-uploaded_file = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+uploaded_file = st.file_uploader("Choose a CSV file", accept_multiple_files=False)
 if uploaded_file is not None:
   dataframe = pd.read_csv(uploaded_file)
