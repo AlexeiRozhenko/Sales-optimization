1ab2f86189e31609f88543e886ac213c391955f2
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-05 17:02:14 +0300

    Update 02_📈_Sales Report.py

--- pages/02_📈_Sales Report.py
+++ pages/02_📈_Sales Report.py
@@ -3,3 +3,5 @@ import plotly.express as px
 
 st.set_page_config(page_title="Sales report", page_icon="📈")
 
+with st.sidebar:
+  uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
