3d3656b5fcccd7ee717de0e9a7082a82c264aa26
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-05 17:22:58 +0300

    Update 02_📈_Sales Report.py

--- pages/02_📈_Sales Report.py
+++ pages/02_📈_Sales Report.py
@@ -4,6 +4,13 @@ import plotly.express as px
 
 st.set_page_config(page_title="Sales report", page_icon="📈")
 
+st.download_button(
+                  label= 'Download Sample File',
+                  data = xlsx,
+                  file_name='sample_file.xlsx',
+                  use_container_width=True
+                  )
+
 uploaded_file = st.file_uploader("Choose an XLSX file", accept_multiple_files=False)
 if uploaded_file is not None:
   df = pd.read_excel(uploaded_file)
