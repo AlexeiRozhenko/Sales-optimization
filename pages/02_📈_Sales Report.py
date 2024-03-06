8ce864fb05d21f1c2422feef89e9f02207b67c4b
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-06 11:27:42 +0300

    Update 02_📈_Sales Report.py

--- pages/02_📈_Sales Report.py
+++ pages/02_📈_Sales Report.py
@@ -7,13 +7,20 @@ import os
 st.set_page_config(page_title="Sales report", page_icon="📈")
 st.title("Sales BI-dashboard")
 
+@st.cache_data
+def load_data(name, mode):
+    with open(name, mode) as file:
+      return file
+  
 with st.sidebar:
-  with open('sample_table.xlsx','rb') as file:
+  load_data('sample_table.xlsx','rb'):
     st.download_button(label= 'Download Sample File',
                       file_name='sample_table.xlsx',
                       data=file,
                       use_container_width=False)
   
   uploaded_file = st.file_uploader("Choose an XLSX file", accept_multiple_files=False)
-  if uploaded_file is not None:
-    df = pd.read_excel(uploaded_file)
+if uploaded_file is not None:
+  df = pd.read_excel(uploaded_file)
+  st.dataframe(df)
+
