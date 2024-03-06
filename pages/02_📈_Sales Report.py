485dc63512543fdaa914b8c70c0372e532d2362f
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-06 11:16:34 +0300

    Update 02_📈_Sales Report.py

--- pages/02_📈_Sales Report.py
+++ pages/02_📈_Sales Report.py
@@ -6,12 +6,13 @@ import os
 
 st.set_page_config(page_title="Sales report", page_icon="📈")
 
-with open('sample_table.xlsx','rb') as file:
-  st.download_button(label= 'Download Sample File',
-                    file_name='sample_table.xlsx',
-                    data=file,
-                    use_container_width=False)
-
-uploaded_file = st.file_uploader("Choose an XLSX file", accept_multiple_files=False)
-if uploaded_file is not None:
-  df = pd.read_excel(uploaded_file)
+with st.sidebar():
+  with open('sample_table.xlsx','rb') as file:
+    st.download_button(label= 'Download Sample File',
+                      file_name='sample_table.xlsx',
+                      data=file,
+                      use_container_width=False)
+  
+  uploaded_file = st.file_uploader("Choose an XLSX file", accept_multiple_files=False)
+  if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
