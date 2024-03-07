2ac41e66f3340b0180381d8ac07aa4ef837d0a83
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-07 19:30:08 +0300

    Update 02_📈_Sales Report.py

--- pages/02_📈_Sales Report.py
+++ pages/02_📈_Sales Report.py
@@ -23,7 +23,7 @@ def pie_chart(df, values, names, title):
     height=280,
     legend=dict(font=dict(size=10, 
     orientation="h",
-    entrywidth=50,,
+    entrywidth=50,
     y=1.02,
     x=1
 ))
