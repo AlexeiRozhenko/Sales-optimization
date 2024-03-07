c9dc58736af04c6f02a1d1fab42be673b3f6c6df
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-07 20:24:11 +0300

    Update 02_📈_Sales Report.py

--- pages/02_📈_Sales Report.py
+++ pages/02_📈_Sales Report.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from datetime import date
-import openpyxl
+import openpyxl, datetime
 
 st.set_page_config(page_title="Sales report", page_icon="📈")
 st.header("Sales report")
