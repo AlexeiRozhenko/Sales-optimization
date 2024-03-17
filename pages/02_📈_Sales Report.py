5b1683ee5cd69fd5e689542a87057ef8d3db6f97
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-17 16:50:57 +0300

    Update 02_📈_Sales Report.py

--- pages/02_📈_Sales Report.py
+++ pages/02_📈_Sales Report.py
@@ -67,7 +67,7 @@ if uploaded_file is not None:
 
   # second row of graphs
   col1, col2, col3 = st.columns(3)
-  if d[1]:
+  if d[1] != "":
     with col1:
        pie_chart(df, "sales", "city", "Sales by city")
   
