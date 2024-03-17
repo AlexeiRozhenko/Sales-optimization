903359ec8a0c5dc411d65fac67d9786c5e2bce40
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-17 15:51:23 +0300

    Update 03_🌅Image generation.py

--- pages/03_🌅Image generation.py
+++ pages/03_🌅Image generation.py
@@ -1,11 +1,27 @@
 import streamlit as st
+import requests
+import io
+from PIL import Image
+# with st.sidebar:
+#     option = st.selectbox(
+#         'Which model would you like to use?',
+#         ('GigaChat', 'Another one (no Russian)'))
+# if option == "GigaChat":
 
-with st.sidebar:
-    option = st.selectbox(
-        'Which model would you like to use?',
-        ('GigaChat', 'Another one (no Russian)'))
+prompt = st.text_input('What would you like to draw?', 'Chocolate bar design, cinematic')
 
-if option == "GigaChat":
-  title = st.text_input('What would you like to draw?', 'Chocolate bar design, cinematic')
+API_URL = "https://api-inference.huggingface.co/models/digiplay/AbsoluteReality_v1.8.1"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.content
+image_bytes = query({
+	"inputs": f"{prompt}",
+})
+# You can access the image with PIL.Image for example
+image = Image.open(io.BytesIO(image_bytes))
+
+st.image(image, caption='Prompt result')
   
 
