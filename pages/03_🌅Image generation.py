4dbb02dd4725de39e25e8f3b9f9c767d8883e5fe
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-17 15:57:22 +0300

    Update 03_🌅Image generation.py

--- pages/03_🌅Image generation.py
+++ pages/03_🌅Image generation.py
@@ -8,20 +8,20 @@ from PIL import Image
 #         ('GigaChat', 'Another one (no Russian)'))
 # if option == "GigaChat":
 
-prompt = st.text_input('What would you like to draw?', 'Chocolate bar design, cinematic')
-
 API_URL = "https://api-inference.huggingface.co/models/digiplay/AbsoluteReality_v1.8.1"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.content
-image_bytes = query({
+	
+prompt = st.text_input('What would you like to draw?', 'Chocolate bar design, cinematic')
+if st.button("Generate image"):
+  image_bytes = query({
 	"inputs": f"{prompt}",
 })
-# You can access the image with PIL.Image for example
-image = Image.open(io.BytesIO(image_bytes))
 
+image = Image.open(io.BytesIO(image_bytes))
 st.image(image, caption='Prompt result')
   
 
