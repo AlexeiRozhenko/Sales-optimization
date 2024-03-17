e0d5ff4826668b86f0cc5c0662b07b64a46a8044
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-17 17:59:44 +0300

    Update 01_💡_Assistant.py

--- pages/01_💡_Assistant.py
+++ pages/01_💡_Assistant.py
@@ -29,7 +29,7 @@ if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
             response = generate_response(prompt, hf_email, hf_pass) 
-            st.write(response)
+            st.markdown(response)
         message = {"role": "assistant", "content": response}
     st.session_state.messages.append({"role": "assistant", "content": response})
 
