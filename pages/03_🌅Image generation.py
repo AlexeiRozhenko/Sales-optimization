c7897488d58856ae14a82568daf30b3aae3d82ec
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-17 15:20:58 +0300

    Update 03_🌅Image generation.py

--- pages/03_🌅Image generation.py
+++ pages/03_🌅Image generation.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
-option = st.selectbox(
-    'Which model would you like to use?',
-    ('GigaChat', 'Another one (no Russian)'))
+with st.sidebar:
+    option = st.selectbox(
+        'Which model would you like to use?',
+        ('GigaChat', 'Another one (no Russian)'))
 
 if option == "GigaChat":
   title = st.text_input('What would you like to draw?', 'Chocolate bar design, cinematic')
