7125dc6e6444eab203209379c64d0b59aeb241d4
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-22 13:51:35 +0300

    Update 04_Testus.py

--- pages/04_Testus.py
+++ pages/04_Testus.py
@@ -5,28 +5,33 @@ from langchain.schema import ChatMessage
 CREDENTIALS = st.secrets["CREDENTIALS"]
 chat = GigaChat(credentials=CREDENTIALS, verify_ssl_certs=False)
 
+# Initialize chat history
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "Hello! How can I help you?"}]
+    st.session_state.messages = [
+        ChatMessage(
+            role="system",
+            content="Ты - умный ИИ ассистент, который специализируется на экономических, технических вопросах развития шоколадного бизнеса.",
+        ),
+        ChatMessage(role="assistant", content="Как я могу помочь вам?"),
+    ]
 
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    with st.chat_message(message.role):
+        st.markdown(message.content)
 
-if prompt := st.chat_input("Enter your prompt here"):
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            response = chat(st.session_state["messages"])
-            # response = chat.generate([ChatMessage(content=prompt)])
-            st.markdown(response.content)
-        message = {"role": "assistant", "content": response.content}
+if prompt := st.chat_input():
+    message = ChatMessage(role="user", content=prompt)
     st.session_state.messages.append(message)
 
+    with st.chat_message(message.role):
+        st.markdown(message.content)
 
+    message = ChatMessage(role="assistant", content="")
+    st.session_state.messages.append(message)
 
-
-  
+    with st.chat_message(message.role):
+        message_placeholder = st.empty()
+        for chunk in chat.stream(st.session_state.messages):
+            message.content += chunk.content
+            message_placeholder.markdown(message.content + "▌")
+        message_placeholder.markdown(message.content)
