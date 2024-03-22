99fabf60fdea478ed7f4981e7e841340a058b230
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-22 13:57:27 +0300

    Update 04_Testus.py

--- pages/04_Testus.py
+++ pages/04_Testus.py
@@ -12,26 +12,26 @@ if "messages" not in st.session_state:
             role="system",
             content="Ты - умный ИИ ассистент, который специализируется на экономических, технических вопросах развития шоколадного бизнеса.",
         ),
-        ChatMessage(role="assistant", content="Как я могу помочь вам?"),
+        ChatMessage(role="assistant", content="How can I help you?"),
     ]
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        st.markdown(message.content)
+        st.markdown(message["content"])
 
 if prompt := st.chat_input():
     message = ChatMessage(role="user", content=prompt)
     st.session_state.messages.append(message)
 
-    with st.chat_message(message.role):
-        st.markdown(message.content)
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
     message = ChatMessage(role="assistant", content="")
     st.session_state.messages.append(message)
 
-    with st.chat_message(message.role):
+    with st.chat_message(message["role"]):
         message_placeholder = st.empty()
         for chunk in chat.stream(st.session_state.messages):
-            message.content += chunk.content
-            message_placeholder.markdown(message.content + "▌")
-        message_placeholder.markdown(message.content)
+            message["content"] += chunk.content
+            message_placeholder.markdown(message["content"] + " ")
+        message_placeholder.markdown(message["content"])
