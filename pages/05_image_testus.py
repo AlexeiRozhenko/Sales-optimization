fd45db84890860da784ddfbc2b2191db6bef66b0
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-26 12:10:38 +0300

    Update 05_image_testus.py

--- pages/05_image_testus.py
+++ pages/05_image_testus.py
@@ -1,23 +1,68 @@
 import streamlit as st
 import requests
-import io
-from PIL import Image
-# with st.sidebar:
-#     option = st.selectbox(
-#         'Which model would you like to use?',
-#         ('GigaChat', 'Another one (no Russian)'))
-# if option == "GigaChat":
-API_TOKEN = st.secrets["TOKEN"]
-API_URL = "https://api-inference.huggingface.co/models/digiplay/AbsoluteReality_v1.8.1"
-headers = {"Authorization": f"Bearer {API_TOKEN}"}
+import json
+import time
+# import io
+# from PIL import Image
 
-def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.content
+class Text2ImageAPI:
+
+    def __init__(self, url, api_key, secret_key):
+        self.URL = url
+        self.AUTH_HEADERS = {
+            'X-Key': f'Key {api_key}',
+            'X-Secret': f'Secret {secret_key}',
+        }
+
+    def get_model(self):
+        response = requests.get(self.URL + 'key/api/v1/models', headers=self.AUTH_HEADERS)
+        data = response.json()
+        return data[0]['id']
+
+    def generate(self, prompt, model, images=1, width=1024, height=1024):
+        params = {
+            "type": "GENERATE",
+            "numImages": images,
+            "width": width,
+            "height": height,
+            "generateParams": {
+                "query": f"{prompt}"
+            }
+        }
+
+        data = {
+            'model_id': (None, model),
+            'params': (None, json.dumps(params), 'application/json')
+        }
+        response = requests.post(self.URL + 'key/api/v1/text2image/run', headers=self.AUTH_HEADERS, files=data)
+        data = response.json()
+        return data['uuid']
+
+    def check_generation(self, request_id, attempts=10, delay=10):
+        while attempts > 0:
+            response = requests.get(self.URL + 'key/api/v1/text2image/status/' + request_id, headers=self.AUTH_HEADERS)
+            data = response.json()
+            if data['status'] == 'DONE':
+                return data['images']
+
+            attempts -= 1
+            time.sleep(delay)
+
+
+if __name__ == '__main__':
+    api = Text2ImageAPI('https://api-key.fusionbrain.ai/', 'KANDINSKY_API', 'KANDINSKY_SECRET')
+    model_id = api.get_model()
+    uuid = api.generate(f"{prompt}", model_id)
+    images = api.check_generation(uuid)
+    print(images)
+
+# def query(payload):
+# 	response = requests.post(API_URL, headers=headers, json=payload)
+# 	return response.content
 	
-prompt = st.text_input('What would you like to draw?', 'Chocolate bar design, cinematic')
-if st.button("Generate image"):
-  image_bytes = query({
-	"inputs": f"{prompt}",})
-  image = Image.open(io.BytesIO(image_bytes))
-  st.image(image, caption='Prompt result')
+# prompt = st.text_input('What would you like to draw?', 'Chocolate bar design, cinematic')
+# if st.button("Generate image"):
+#   image_bytes = query({
+# 	"inputs": f"{prompt}",})
+#   image = Image.open(io.BytesIO(image_bytes))
+#   st.image(image, caption='Prompt result')
