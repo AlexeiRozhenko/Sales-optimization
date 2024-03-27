3dda0df63a58429e049a3697d0045b815efb5669
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-27 17:11:44 +0300

    Update 👋_Homepage.py

--- 👋_Homepage.py
+++ 👋_Homepage.py
@@ -7,7 +7,7 @@ st.subheader("Comprehensive AI application ✨")
 st.markdown(
     """
     **What can you do here?**
-    - Enjoy talking with Gigachat, Sber text-to-text neural network, about everything you want. It was deliberately programmed to answer questions about chocolate business development 
+    - Enjoy talking with Gigachat neural network about everything you want. It was deliberately programmed to answer questions about chocolate business development 
     (supports English and Russian)
     - Optimize your sales. Just arrange your data as a template and here you are, detailed BI analytics served specially for you
     - Get inspired by Kandinsky neural network and its vision (supports English and Russian)
