c139d0fe4e5fc856ad87de9806a8b140b56447b4
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-27 17:18:54 +0300

    Update 👋_Homepage.py

--- 👋_Homepage.py
+++ 👋_Homepage.py
@@ -10,6 +10,7 @@ st.markdown(
     - **AI assistant**: Enjoy talking with Gigachat neural network about everything you want. It was deliberately programmed to answer questions about chocolate business development 
     - **Sales report**: Optimize your sales. Just arrange your data as a template and here you are, detailed BI analytics served specially for you
     - **Design ideas**: Get inspired by Kandinsky neural network and its vision
+   
     **Supported languages:**
     - English
     - Russian
