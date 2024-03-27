5e432f5fcd908fd3ef683245fb26628c4a3ea834
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-03-27 17:30:12 +0300

    Update 👋_Homepage.py

--- 👋_Homepage.py
+++ 👋_Homepage.py
@@ -7,7 +7,7 @@ st.subheader("Comprehensive AI application ✨")
 st.markdown(
     """
     **What can it do for you?**
-    - **AI assistant**: Enjoy talking with Gigachat neural network about everything you want. It was deliberately programmed to answer questions about chocolate business development 
+    - **💡 AI assistant**: Enjoy talking with Gigachat neural network about everything you want. It was deliberately programmed to answer questions about chocolate business development 
     - **Sales report**: Optimize your sales. Just arrange your data as a template and here you are, detailed BI analytics served specially for you
     - **Design ideas**: Get inspired by Kandinsky neural network and its vision
    
